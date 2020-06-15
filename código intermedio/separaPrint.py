f43c4668a3f152a63744958bc3208ee1066137f6
crist-start <cristart.cs@gmail.com>
2020-06-15 16:08:26 -0500

    gitignore

---
+++ código intermedio/separaPrint.py
@@ -0,0 +1,28 @@
+def esCadena(a=""):
+    return a[0]=='"' and a[-1]=='"'
+
+def separaPrint(a=[]):
+    pr=['print','(']
+    salida=[]
+    for i in a:
+        if esCadena(i):
+            pr.append(i)
+        elif i==',':
+            pr.extend([')',';'])
+            salida.append(pr)
+            pr=['print','(']
+        elif i==')':
+            pr.append(i)
+        elif i==';':
+            pr.append(i)
+            salida.append(pr)
+            if(a[0]=='println'):
+                salida.append(['print','(','\n',')',';'])
+    return salida;
+
+s=separaPrint(['println','(','"dato"',',','"dato2"',',','"dato3"',')',';'])
+t=separaPrint(['println','(','"dato"',')',';'])
+for i in s:
+    print (i)
+print(t[0])
+print(t[1])
