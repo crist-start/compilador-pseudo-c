e9d814d99fffdb0f1ff20584815590335bc60065
CesarWit <ceswitcal@gmail.com>
2020-06-18 23:15:41 -0500

    Programa final gestionando variables como objetos

--- generación de codigo/programa.py
+++ generación de codigo/programa.py
@@ -1,32 +1,17 @@
-def Instruccion(ins):
-	CODIGO = open("Codigo.txt","a")
-	CODIGO.write("\n"+cadena)
-	CODIGO.close
+def Instruccion(ins): #Limpiar en caso de error
+	COMPILADOR = open("Codigo.txt","a")
+	COMPILADOR.write("\n"+ins)
+	COMPILADOR.close
 
-def RevisarTipo(var):
-	if variables[0] == var:
-		return variables[1]
-	else:
-		Error = "La variable {} no se encuentra en la tabla de variables".format(var)
-		Registro(Error)
-		return None
-
-def DirVar(var): #Obtener la dirección de la variable
-	if variables[0]==var:
-		return variables[3]
-	else:
-		Error = "La variable {} no esta en la tabla.".format(var)
-		Registro(Error)
-		return None
-
-def Registro(cadena):
+def Registro(cadena): 
 	REG = open("Registro.txt","a")
 	REG.write("\n"+cadena)
 	REG.close
 
 def Leer(var):
 	cadena = ""
-	tipo=RevisarTipo(var)
+	#tipo=RevisarTipo(var)
+	tipo=var.tipo
 	if tipo == "int":
 		cadena = "IN1 {};".format(var)
 		Instruccion(cadena)
@@ -49,7 +34,8 @@ def ImprimirCadena(cadena):
 
 def ImprimirVariable(var):
 	cadena = ""
-	tipo=RevisarTipo(var)
+	#tipo=RevisarTipo(var)
+	tipo = var.tipo
 	if tipo == "int":
 		cadena = "IN5 {};".format(var)
 		Instruccion(cadena)
@@ -69,15 +55,18 @@ def ImprimirVariable(var):
 def AsignarValVar(var,value): #Asignación de la forma a=10
 	cadena = "LDV {};".format(value)
 	Instruccion(cadena)
-	direccion = DirVar(var)
+	#direccion = DirVar(var)
+	direccion = var.direccion
 	cadena = "STA {};".format(direccion)
 	Instruccion(cadena)
 
 def AsignarVarVar(var1,var2): #Asignación de la forma b=a
-	dirvar2 = DirVar(var2)
+	#dirvar2 = DirVar(var2)
+	dirvar2 = var2.direccion
 	cadena = "LDA {};".format(dirvar2)
 	Instruccion(cadena)
-	dirvar1 = DirVar(var1)
+	#dirvar1 = DirVar(var1)
+	dirvar1 = var1.direccion
 	cadena = "STA {};".format(dirvar1)
 	Instruccion(cadena)
 
@@ -98,10 +87,12 @@ def Operacion(cadena):
 			cad = ""
 		else:
 			cad = cad + temp[i]
-	OP1 = DirVar(Operando1)
+	#OP1 = DirVar(Operando1)
+	OP1 = Operando1.direccion
 	cadena = "LDA {};".format(OP1)
 	Instruccion(cadena)
-	OP2 = DirVar(Operando2)
+	#OP2 = DirVar(Operando2)
+	OP2 = Operando2.direccion
 	if Operador == "+":
 		cadena = "ADD {};".format(OP2)
 	elif Operador == "-":
@@ -115,5 +106,48 @@ def Operacion(cadena):
 		Registro(Error)
 		cadena = ""
 	Instruccion(cadena)
-	
+	#TipoOp1 = RevisarTipo(Operando1)
+	TipoOp1 = Operando1.tipo
+	#TipoOp2 = RevisarTipo(Operando2)
+	TipoOp2 = Operando2.tipo
+	if ((TipoOp1 == "int") and (TipoOp2 == "int") and (Operador in "+-*")):
+		TipoRes = "int"
+	else:
+		TipoRes = "float"
+	DirLibre = (variables[-1][-1])+1
+	#variables(Resultado,TipoRes,0,DirLibre)
+	Resultado.tipo = TipoRes
+	#Result = DirVar(Resultado)
+	Result = Resultado.direccion
+	cadena = "STA {};".format(Result)
+
+def ManejoFor(cadena): #Tienes que recibir cadena desde for hasta la llave de cierre
+	texto = cadena.splitlines()
+	texto.remove("")
+	condicion = texto[0][4:-1]
+	#inicio
+	#fin
+	bloque = texto[2:-1]
+	for i in range(0,len(bloque)):
+		bloque[i] = bloque[i].replace("\t","")
+	cantidad = bloque.count("")
+	for i in range(0,cantidad):
+		bloque.remove("")
+	for i in range(0,len(bloque)):
+		EvaluarInstruccion(bloque[i])
+
+def EvaluarInstruccion(inst):
+	pass
+
+def LeerCodigo(archivo):
+	codigo = open(archivo,"r")
+	temp = codigo.read()
+	codigo.close()
+	codigo = temp.splitlines()
+	for renglon in range(0,len(codigo)):
+
+def GenerarCodigo(CodigoIntermedio2x2): #Esta funcion se va a ir a main
+	pass
+
+#Este codigo queda obsoleto con los ajustes a el manejo de variables
 
---
+++ generación de codigo/programa2.py
@@ -0,0 +1,137 @@
+def ACompilador(instruccion):
+	Comp = open("Codigo.txt","a")
+	Comp.write("\n"+instruccion)
+	Comp.close
+
+def Registro(cadena):
+	Reg = open("Registro.txt","a")
+	Reg.write("\n"+cadena)
+	Reg.close()
+
+def Leer(token):
+	cadena = ""
+	var = token[2]
+	tipo = var.tipo
+	if tipo == "int":
+		cadena = "IN1 {};".format(var)
+		Instruccion(cadena)
+	elif tipo == "float":
+		cadena = "IN2 {};".format(var)
+		Instruccion(cadena)
+	elif tipo == "char":
+		cadena = "IN3 {};".format(var)
+		Instruccion(cadena)
+	elif tipo == "string":
+		cadena = "IN4 {};".format(var)
+		Instruccion(cadena)
+	else:
+		Error = "No se pudo manejar(leer) el tipo de dato de {}.".format(var)
+		Registro(Error)
+
+def ImprimirCadena(texto):
+	cadena = "IN9 {};".format(texto)
+	ACompilador(cadena)
+
+def ImprimirVariable(variable):
+	cadena = ""
+	tipo = variable.tipo
+	if tipo == "int":
+		cadena = "IN5 {};".format(variable)
+	elif tipo == "float":
+		cadena = "IN6 {};".format(variable)
+	elif tipo == "char":
+		cadena = "IN7 {};".format(variable)
+	elif tipo == "string":
+		cadena = "IN8 {};".format(variable)
+	else:
+		Error = "No se pudo manejar(imprimir) el tipo de dato de {}.".format(variable)
+		Registro(Error)
+	ACompilador(cadena)
+
+def AsignarValVar(variable,valor):
+	cadena = "LDV {};".format(valor)
+	ACompilador(cadena)
+	cadena = "STA {};".format(variable.direccion)
+	ACompilador(cadena)
+
+def AsignarVarVar(var1,var2):
+	cadena = "LDA {};".format(var2.direccion)
+	ACompilador(cadena)
+	cadena = "STA {};".format(var1.direccion)
+	ACompilador(cadena)
+
+def AsignarTrigon(token):
+	if token[4][0] in "0123456789":
+		dir = token[4]
+	else:
+		dir = token[4].direccion
+	if token[2] == "sin":
+		cadena = "SIN {};".format(dir)
+	elif token[2] == "cos":
+		cadena = "COS {};".format(dir)
+	elif token[2] == "tan":
+		cadena = "TAN {};".format(dir)
+	else:
+		Error = "La funcion trigonometrica {} no es procesable.".format(token[2])
+		Registro(Error)
+		cadena "LDA {};".format(dir)
+	ACompilador(cadena)
+	cadena = "STA {};".format(token[0].direccion)
+
+def Aritmetica(token):
+	OP1 = token[2]
+	OP2 = token[4]
+	cadena = "LDA {};".format(OP1.direccion)
+	ACompilador(cadena)
+	if token[3] == "+":
+		cadena = "ADD {};".format(OP2.direccion)
+	elif token[3] == "-":
+		cadena = "SUB {};".format(OP2.direccion)
+	elif token[3] == "*":
+		cadena = "MUL {};".format(OP2.direccion)
+	elif token[3] == "/":
+		cadena = "DIV {};".format(OP2.direccion)
+	else:
+		Error = "El operando {} no se pudo utilizar para operar {} y {}.".format(token[3],OP1,OP2)
+		Registro(Error)
+		cadena = ""
+	ACompilador(cadena)
+	Resultado = token[0]
+	cadena = "STA {};".format(Resultado.direccion)
+	ACompilador(cadena)
+
+
+
+def ManejoFor(token):
+	pass
+
+def EvaluarInstruccion(token):
+	if token[0] == "print":
+		if token[2][0] != '"':
+			ImprimirVariable(token[2])
+		else:
+			ImprimirCadena(token[2])
+	elif token[0] == "read":
+		Leer(token)
+	elif token[1] == "=":
+		if (("sin" in token[2]) or ("cos" in token[2]) or ("tan" in token[2])):
+			AsignarTrigon(token)
+		elif token[3] == ";":
+			if token[2][0] in "0123456789":
+				AsignarValVar(token[0],token[2])
+			else:
+				AsignarVarVar(token[0],token[2])
+		else:
+			Aritmetica(token)
+	elif #CondicionFor:
+		pass
+	else:
+		pass
+
+def GenerarCodigo(CodigoIntermedio):
+	for renglon in range(0,len(CodigoIntermedio)):
+		if "condicionfor":
+			pass #funcionfor
+		else:
+			EvaluarInstruccion(CodigoIntermedio[renglon])
+			#Funcion pendiente para imprimir token (instr = CodigoIntermedio[renglon])
